bd29a19458a7e191c9e7cb951dccd81770e7610d
D.c <63380156+ucscDc98@users.noreply.github.com>
2022-04-26 13:48:24 -0400

    swp ui

---
+++ Cat Cafe Simulator/Assets/Script/buttonManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonManager : MonoBehaviour
+{   
+    public GameObject player;
+    public GameObject action1;
+    public GameObject action2;
+    public GameObject action3;
+
+    int p;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        p = GameObject.FindWithTag(player).GetComponent<Character>(CatStatus).favorability;
+
+        if(p < 4)
+        {
+            action1.SetActive(true);
+            action2.SetActive(false);
+            action3.SetActive(false);
+        }
+
+        if(p = 4)
+        {
+            action1.SetActive(false);
+            action2.SetActive(true);
+            action3.SetActive(false);
+        }
+
+        if(p > 4)
+        {
+            action1.SetActive(false);
+            action2.SetActive(false);
+            action3.SetActive(true);
+        }
+
+    }
+}
