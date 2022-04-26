ce310a8818ce22819ac0d7f70d069b761cb80b5b
Ili <sion9801@gmail.com>
2022-04-26 13:34:04 -0400

    animation

--- Cat Cafe Simulator/Assets/Script/Button1.cs
+++ Cat Cafe Simulator/Assets/Script/Button1.cs
@@ -2,11 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-<<<<<<< HEAD:Cat Cafe Simulator/Assets/Script/Button3.cs
-public class Button3 : MonoBehaviour
-=======
 public class Button1 : MonoBehaviour
->>>>>>> e6e0e75e60ba37cc6f78d4e5c423a225f3ab06e0:Cat Cafe Simulator/Assets/Script/Button1.cs
 {
 	public Button yourButton;
 	void Start()
